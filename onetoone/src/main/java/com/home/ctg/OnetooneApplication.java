5ee08c51605345d7b3f9979dbc4f7f3b3ec3b705
123tharun <123tharun@gmail.com>
2018-11-30 20:36:09 -0500

    Initial commit

---
+++ onetoone/src/main/java/com/home/ctg/OnetooneApplication.java
@@ -0,0 +1,12 @@
+package com.home.ctg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnetooneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnetooneApplication.class, args);
+	}
+}
